6ab88a0986a7c18e1018f3a2ece4d876006aeba4
Mount-Blanc <mountblanc87@gmail.com>
2022-05-29 09:42:56 -0700

    finished file structre

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
+import { Navbar, Footer, Sidebar, ThemeSettings} from './components'
+import { Ecommerce, Orders, Calendar, Employees, Stacked,
+Pyramid, Customers, Kanban, Area, Bar, Pie, Financial,
+ColorPicker, ColorMapping, Editor} from './pages';
+
 import './App.css';
 
 const App = () => {
@@ -47,7 +52,7 @@ const App = () => {
                   Navbar
                   </div>
                 </div>
-
+              </div>
             <div>
               <Routes>
                 {/* Dashboard */}
@@ -75,7 +80,6 @@ const App = () => {
                 <Route path="/stacked"  element="Stacked"/>     
               </Routes>
             </div>
-          </div>
             </BrowserRouter>
          </div>
     )
