07f1d297398c869184dd52c2c3ef82913bfc28b7
Mount-Blanc <mountblanc87@gmail.com>
2022-05-30 11:34:16 -0700

    fixed link typo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,8 +8,10 @@ import { links } from '../data/dummy';
 
 const Sidebar = () => {
   const activeMenu =true;
-  const activeLink = `flex items-center
+  const activeLink = `flex i
+  tems-center
   gap-5 pl-3 pt-3 pb-2.5 rounded -lg text-white text-md m-2`;
+  
   const normalLink =`flex items-center
   gap-5 pl-3 pt-3 pb-2.5 rounded -lg text-white text-md 
   text-gray-700 dark:text-gray-200 dark:hover:text-black 
@@ -17,7 +19,8 @@ const Sidebar = () => {
 
   return (
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
-      {activeMenu && (<>
+      {activeMenu && (
+      <>
       <div className="flex justify-between items-center">
       <Link to='/' onClick={ () => {} }
       className="items-center gap-2 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:white text-slate-900">
@@ -28,7 +31,7 @@ const Sidebar = () => {
           <button type="button"
           onClick={() => {}}
           className= "text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden">
-            
+
             <MdOutlineCancel />
           </button>
         </TooltipComponent>
@@ -39,25 +42,26 @@ const Sidebar = () => {
             <p className="text-gray-400 m-3 mt-4 uppercase"> 
               {item.title}
                 </p>
-                {item.links.mapp ((Link) => (
+                {item.links.map ((Link) => (
                   <NavLink
                   to={`/${Link.name}`}
-                  key={links.name}
+                  key={Link.name}
                   onClick={() => {}}
                   className= {({isActive}) =>
                 isActive ? activeLink :
               normalLink}
                   >
-                    {links.icon}
+                    {Link.icon}
                     <span className='capitalize'>
-                    {links.name}
+                    {Link.name}
                     </span>
                   </NavLink>
                 ))}
             </div>
-        ))}
+           ))}
         </div>
-        </>)}
+        </>
+        )}
     </div>
   )
 }
