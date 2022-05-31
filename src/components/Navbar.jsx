afc0e8b113c0d6161e5688fb4e7705702c8f6ac9
Mount-Blanc <mountblanc87@gmail.com>
2022-05-31 16:20:51 -0700

    added styling to button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,16 @@ const NavButton =({ title, customFunc, icon,
   color, dotColor }) => (
     <TooltipComponent content ={title}
     position="Bottom-center">
-      <button></button>
+      <button type="button" onClick={customFunc}
+      style={( color )}
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray">
+        <span style={{ background: dotColor}}
+        className="absolute inline-flex
+        rounded-full h-2 w-2 right-2 top-2"
+        >
+          {icon}
+        </span>
+      </button>
     </TooltipComponent>
   )
 
@@ -27,7 +36,10 @@ const Navbar = () => {
 
   return (
     <div className= "flex justify-between p-2 md:mx-6 relative">
-
+      <NavButton title= "Menu" customFunc={() =>
+      setActiveMenu ((prevActiveMenu) =>
+      !prevActiveMenu)} color="blue"  icon=
+      {<AiOutlineMenu />} />
     </div>
   )
 }
