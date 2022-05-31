d8892f079728e86778a019704973f934464e6f5a
Mount-Blanc <mountblanc87@gmail.com>
2022-05-31 15:59:09 -0700

    added imports to navbar.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,14 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { AiOutlineMenu } from 'react-icons/ai';
+import { AiFillShopping } from 'react-icons/fi';
+import {BschatLeft} from 'react-icons/bs';
+import { RiNotification3Line } from 'react-icons/ri';
+import {MDkeyboardArrowDown }from 'react-cons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import avatar from '../data/avatar.jpg';
+import { Cart, Chat, Notification, UserProfile }
+from '.';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Navbar = () => {
   return (
